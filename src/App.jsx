27decfc18256ca0f11c80bc8a1dd71347fe977df
UmmIt <git@ummit.dev>
2025-03-17 08:04:36 +0800

    style(modal): enhance GPG modal appearance and button styles
    - Updated modal background to be transparent with a backdrop blur effect.
    - Improved button styles for download and close actions (No more btn style.)

--- src/App.jsx
+++ src/App.jsx
@@ -117,7 +117,7 @@ function App() {
 
       {/* Modal Component */}
       <dialog id="gpg_modal" className="modal">
-        <div className="modal-box bg-base-100 rounded-lg p-11 w-full max-w-2xl max-h-[75vh] overflow-auto">
+        <div className="modal-box bg-transparent rounded-lg p-11 w-full max-w-2xl max-h-[75vh] overflow-auto backdrop-blur-md rounded-xl p-6 border border-gray-700/30 hover:border-myPink1 transition-all duration-650 hover:shadow-lg hover:shadow-myPink1/20">
           <div className="flex justify-between items-center mb-4">
             <h3 className="text-lg font-bold italic">
               ❯ curl https://links.ummit.dev/info/UmmIt.gpg | less
@@ -125,21 +125,23 @@ function App() {
             <div className="flex gap-2">
               <button
                 onClick={handleDownload}
-                className="btn btn-ghost btn-sm tooltip tooltip-top"
+                className="bg-transparent border border-gray-700/30 rounded-lg p-2 tooltip tooltip-top"
                 data-tip="Download GPG key"
               >
                 <FiDownload className="w-5 h-5" />
               </button>
+
               <button
                 onClick={() => document.getElementById("gpg_modal").close()}
-                className="btn btn-ghost btn-sm tooltip tooltip-top"
+                className="bg-transparent border border-gray-700/30 rounded-lg p-2 tooltip tooltip-top"
                 data-tip="Close"
               >
                 <IoMdClose className="w-5 h-5" />
               </button>
             </div>
           </div>
-          <pre className="whitespace-pre-wrap font-mono text-sm bg-base-200 p-4 rounded-lg overflow-auto max-w-full break-words">
+
+          <pre className="whitespace-pre-wrap font-mono text-sm bg-transparent p-4 rounded-lg overflow-auto max-w-full break-words">
             {gpgContent || "Loading GPG key..."}
           </pre>
         </div>
