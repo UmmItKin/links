e5060a15f0c5e8e7309a03d78d9120afc906935e
UmmIt <git@ummit.dev>
2025-04-23 07:35:16 +0800

    fix(ci): refactor Vite config to use dynamic base URL and update workflow to set environment variable

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: process.env.VITE_BASE_URL || '/',
-  server: {
-    historyApiFallback: true,
-  },
+export default defineConfig(({ mode }) => {
+  const base = process.env.VITE_BASE_URL || '/'
+  
+  return {
+    plugins: [react()],
+    base: base,
+    server: {
+      historyApiFallback: true,
+    }
+  }
 })
