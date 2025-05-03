becdad2cd05ec2035f22bdd3b86b611687c3dcdd
UmmIt <git@ummit.dev>
2025-05-03 12:46:36 +0800

    chore(AboutPage): add CTF team information

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { FaCode, FaServer, FaMapMarkerAlt } from 'react-icons/fa';
 import { SiArchlinux } from 'react-icons/si';
 import { MdOutlineSecurity } from "react-icons/md";
+import { FaFlag } from "react-icons/fa6";
 
 interface Interest {
   icon: React.ReactNode;
@@ -53,9 +54,10 @@ const AboutPage: React.FC = () => {
             <p className="text-gray-300 mb-6">
               A quirky tech geek, Arch Linux user, and open-source lover from Hong Kong. 
               I'm a cyber student with a hobby in hacking and penetration testing. 
-              I enjoy working on various tech projects, particularly focusing on GNU/Linux systems, and cybersecurity.
+              I enjoy working on various tech projects, particularly focusing on GNU/Linux systems, and cybersecurity. <br/><br/>
+              I’m also member of a CTF (Capture The Flag) team called <a href="https://ctftime.org/team/303514/" className="text-myPink1 link link-hover" target="_blank">ICEETEA (冰茶)</a> <FaFlag className="inline text-myPink1" />
             </p>
-            
+
             <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
               {interests.map((interest, index) => (
                 <div key={index} className="flex flex-col items-center p-3 bg-gray-700/30 rounded-lg">
