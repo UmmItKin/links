72987f24a0c8baf9930133f0938377ac5817e2cd
UmmIt <git@ummit.dev>
2025-05-17 22:16:40 +0800

    style(GPGModal): improve hover effects and color

--- src/components/GPGModal/index.tsx
+++ src/components/GPGModal/index.tsx
@@ -11,7 +11,7 @@ interface GPGModalProps {
 const GPGModal: React.FC<GPGModalProps> = ({ gpgContent, onClose, onDownload }) => {
   return (
     <dialog id="gpg_modal" className="modal">
-      <div className="modal-box bg-transparent rounded-lg p-11 w-full max-w-2xl max-h-[75vh] overflow-auto backdrop-blur-md rounded-xl p-6 border border-gray-700/30 hover:border-myPink1 transition-all duration-650 hover:shadow-lg hover:shadow-myPink1/20">
+      <div className="modal-box bg-transparent rounded-lg p-11 w-full max-w-2xl max-h-[75vh] overflow-auto backdrop-blur-md rounded-xl p-6 border border-gray-700/30 hover:border-primary transition-all duration-300 hover:shadow-lg hover:shadow-primary/20">
         <div className="flex justify-between items-center mb-4">
           <h3 className="text-lg font-bold italic">
             ❯ curl https://links.ummit.dev/info/UmmIt.gpg | less
@@ -19,7 +19,7 @@ const GPGModal: React.FC<GPGModalProps> = ({ gpgContent, onClose, onDownload }) 
           <div className="flex gap-2">
             <button
               onClick={onDownload}
-              className="bg-transparent border border-gray-700/30 rounded-lg p-2 tooltip tooltip-top"
+              className="bg-transparent border border-gray-700/100 rounded-lg p-2 tooltip tooltip-top duration-300 hover:scale-105 hover:shadow-primary/30 transform hover:border-primary/50"
               data-tip="Download GPG key"
             >
               <FiDownload className="w-5 h-5" />
@@ -27,7 +27,7 @@ const GPGModal: React.FC<GPGModalProps> = ({ gpgContent, onClose, onDownload }) 
 
             <button
               onClick={onClose}
-              className="bg-transparent border border-gray-700/30 rounded-lg p-2 tooltip tooltip-top"
+              className="bg-transparent border border-gray-700/100 rounded-lg p-2 tooltip tooltip-top duration-300 hover:scale-105 hover:shadow-primary/30 transform hover:border-primary/50"
               data-tip="Close"
             >
               <IoMdClose className="w-5 h-5" />
