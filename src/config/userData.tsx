763d2d64f0499e647c74ad4ff079cb2722270ce0
UmmIt <git@ummit.dev>
2025-05-29 09:07:57 +0800

    chore(userData): update imageIcon and wallpaper URLs to shortURL powered by Cloudflare R2 and Sink

--- src/config/userData.tsx
+++ src/config/userData.tsx
@@ -18,11 +18,11 @@ export const userData: UserData = {
   name: "@UmmIt",
   bio: "Cyber Security Student / Arch Linux User / Open Source Lover",
   mail_address: "hi@ummit.dev",
-  imageIcon: "/cute-2.jpg",
+  imageIcon: "https://short.ummit.dev/cutecuteai-icon-1",
   repo: "https://short.ummit.dev/links-repo",
   umami_share_url: "https://short.ummit.dev/links-umami",
   gpg_key: "https://links.ummit.dev/info/UmmIt.gpg",
-  wallpaper: "https://short.ummit.dev/wallpaper-links-01",
+  wallpaper: "https://short.ummit.dev/ai-socute-1"
 };
 
 export const socialButtons: SocialButton[] = [
